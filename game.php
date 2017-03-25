c1136790577458d93b5b27e2bba594e72137c41f
Giovanni Alfredo Garciliano Diaz <giobeatle10@gmail.com>
2017-03-24 19:45:47 -0600

    Añadir comprobación que todos los elementos estén seleccionados en JS

---
+++ game.php
@@ -0,0 +1,43 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Dino Jump Game</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link rel="icon" type="image/svg" href="/svg/favicon.svg" />
+    <meta name="author" content="Giovanni Alfredo Garciliano Díaz" />
+    <meta name="copyright" content="© 2017 Giovanni Alfredo Garciliano Díaz" />
+    <meta name="robots" content="index, follow" />
+    <meta name="description" content="Un port libre y de código abierto en HTML/CSS/JavaScript del juego del dinosaurio de Chrome: el juego" />
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
+    <meta charset="utf-8">
+    <script src="js/main.js" type="text/javascript"></script>
+    <script src="js/snap.js" type="text/javascript"></script><!--Snap.svg-->
+</head>
+<?php
+?>
+<body id="body">
+    <header>
+        <h1>Dino Jump</h1>
+    </header>
+    
+    <aside id="jsrequired">
+        <p>Bien, parece que vuestro navegador no soporta JavaScript, o lo tenés deshabilitado. Habilítalo, o <a href="https://www.mozilla.org/es-MX/firefox/new/">descarga un navegador libre, seguro y actualizado que soporta JavaScript.</a></p>
+    </aside>
+    <aside id="cssrequired">
+        <p>Bien, parece que vuestro navegador no soporta CSS, o lo tenés deshabilitado. Habilítalo, o <a href="https://www.mozilla.org/es-MX/firefox/new/">descarga un navegador libre, seguro y actualizado que soporta CSS.</a></p>
+    </aside>
+    <article id="paneljuego">
+        <section id="opciones">
+            n
+        </section>
+        <svg id="juego">
+        </svg>
+    </article>
+    <footer>
+        <p>Hecho con <img src="img/amor.svg" alt="amor" id="amor" /> desde México.</p>
+        <p>Este juego está publicado bajo la <a rel="license" href="https://www.gnu.org/licenses/gpl.html">GNU GPLv3</a>, así como este sitio web.</p>
+        <p>Copyright © 2017 Giovanni Alfredo Garciliano Díaz</p>
+    </footer>
+    <audio src="ogg/Dino_Jump_Track.ogg" autoplay loop preload="auto"></audio>
+</body>
+</html>
--- js/main.js
+++ js/main.js
@@ -1,4 +1,54 @@
-function jsrequired(){
-    ggg = document.getElementById("jsrequired");
+/* Oculta el aviso de JavaScript a los navegadores que sí lo soportan */
+function jsRequired(){
+    var ggg = document.getElementById("jsrequired");
     ggg.parentNode.removeChild(ggg);
+}
+
+/* Comprueba y envían el formulario */
+function checkForm(){
+    var primero = true;
+    /* Asegura que se haya seleccionado un personaje */
+    var radiobuttons = document.getElementsByName("personaje");
+    for(var i = 0; i < radiobuttons.length; i++) {
+        if (radiobuttons[i].checked == true) {
+            break;
+        } else {
+            if (primero == false){
+                return false;
+            } else {
+                primero = false;
+                continue;
+            }
+        }
+    }
+    /* Asegura que se haya seleccionado un paisaje */
+    primero = true;
+    var radiobuttons2 = document.getElementsByName("paisaje");
+    for(var i = 0; i < radiobuttons2.length; i++) {
+        if (radiobuttons2[i].checked == true) {
+            return true;
+        } else {
+            if (primero == false){
+                return false;
+            } else {
+                primero = false;
+                continue;
+            }
+        }
+    }
+}
+
+function submitForm(){
+    /* Si todo está correcto, se envía el formulario */
+    if (checkForm() == true){
+        ggg = document.getElementById("ajugar");
+        ggg.value = "Enviando...";
+        ggg.form.submit()
+    }
+}
+
+window.onload = function() {
+    jsRequired();
+    buttonsubmit = document.getElementById("ajugar");
+    buttonsubmit.onclick = submitForm;
 }
